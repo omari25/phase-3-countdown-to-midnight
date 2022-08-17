1a868b8e173b487a60231fc1843d4bb8abcf7df0
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-08-17 08:50:24 +0300

    commit tree c977e24b6cc61a3159d7c6d43cd11c81fe46c66c

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown (number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+countdown (12)
+
+def countdown_with_sleep (num)
+    while num > 0
+        puts "#{num} SECOND(S)!"
+        num -= 1
+        sleep 1
+    end
+end
+countdown_with_sleep (4)
